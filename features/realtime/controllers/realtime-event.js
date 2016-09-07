da3316061aa5b8c255b3dd260793de2f8a602c6b
Xavier Boubert <xavier@boubert.fr>
2016-09-07 12:58:48 +0200

    fix(realtime express-bootstap): require $RealTimeService at start

--- features/realtime/controllers/realtime-event.js
+++ features/realtime/controllers/realtime-event.js
@@ -1,9 +1,5 @@
 'use strict';
 
-var path = require('path');
-
-require(path.resolve(__dirname, '../models/realtime-service.js'))();
-
 module.exports = [{
   event: 'update(real-time/events)',
   controller: function($RealTimeService, $socket, $message) {
