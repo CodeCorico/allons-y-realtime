11a16af048016bfe5be02e33667bcfe728bd96ee
Xavier Boubert <xavier@boubert.fr>
2016-09-14 11:40:54 +0200

    refactor(realtime express bootstrap): move code logic inside the module function

--- features/realtime/realtime-express-bootstrap.js
+++ features/realtime/realtime-express-bootstrap.js
@@ -1,9 +1,9 @@
 'use strict';
 
-var path = require('path');
-
-require(path.resolve(__dirname, 'models/realtime-service.js'))();
-
 module.exports = function($done) {
+  var path = require('path');
+
+  require(path.resolve(__dirname, 'models/realtime-service.js'))();
+
   $done();
 };
